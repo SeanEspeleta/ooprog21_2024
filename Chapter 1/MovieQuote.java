6b210141d23186dbe9cf0340ed489921ff9a74e7
Sean Izaac A. Espeleta <seanizaacespeleta@gmail.com>
2024-08-14 22:52:29 +0800

    Add files via upload

---
+++ Chapter 1/MovieQuote.java
@@ -0,0 +1,13 @@
+public class MovieQuote {
+    public static void main(String[] args) {
+        String quote = "With great power comes great responsibility.";
+        String movie = "Spider-Man";
+        String character = "Uncle Ben";
+        int year = 2002;
+
+        System.out.println("Movie: " + movie);
+        System.out.println("Character: " + character);
+        System.out.println("Year: " + year);
+        System.out.println("Quote: " + quote);
+    }
+}
