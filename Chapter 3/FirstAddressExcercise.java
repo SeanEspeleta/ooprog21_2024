9c5a730cdd9ea348707f506e1850fc9773dd7eec
Sean Izaac A. Espeleta <seanizaacespeleta@gmail.com>
2024-09-16 22:27:16 +0800

    Add files via upload

---
+++ Chapter 3/FirstAddressExcercise.java
@@ -0,0 +1,9 @@
+public class FirstAddressExcercise {
+
+    public static void main(String[] args) {
+        System.out.println("SAMSUNG Company");
+        System.out.println("Gangnam station Seoul ");
+        System.out.println("Lee Byung-chul");
+        System.out.println("Company: Samsung Inc.");
+    }
+}
